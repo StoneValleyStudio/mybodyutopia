46cc9ad520563f4e42679befeb7af5305cb5a0d8
Andrew Hickman <hickmaab@clarkson.edu>
2015-02-25 20:29:26 -0500

    Tidied up ready to merge. All systems clear. We're headed to the Master Branch. For peace, love, and unity. God bless America.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,6 @@ var $mb_start = $('header');
 var $header_bottom = $('.header-bottom');
 
 $header_bottom.on('click touchend', function () {
-	$header_bottom.hide();
+	$header_bottom.slideDown(475);
 	$mb_start.slideUp(550);
 });
