ba431b25c7f8b5a6496bde9477f2b487f95e45e3
Andrew Hickman <hickmaab@clarkson.edu>
2015-02-28 20:02:18 -0500

    Added fixed mobile nav as well as Slick Carousel with some initial styling. A few minor bugs regarding media queries but nothing a user would notice.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,39 @@
-// Mobile Start Screen Animation
 
-var $mbStartScreen = $('header');
-var $header_bottom = $('.header-bottom');
+// Mobile Start Screen Animation & Slick Carousel Initiation
+
+var $mbIntro = $('#mobile-intro');
+var $mbIntroBottom = $('.mobile-intro-bottom');
 var $menu = $('#menu');
+var $carousel = $('.carousel');
+var $carousel_val = {
+	autoplay: true,
+	cssEase: "ease-out",
+	fade: true,
+	pauseOnHover: true,
+	autoplaySpeed: 4000,
+	arrows: false
+}
 
-$header_bottom.on('click touchend', function () {
-	$header_bottom.slideDown(475);
-	$mbStartScreen.slideUp(550);
+if ($mbIntro.css('display') == 'block') {
+
+$carousel.hide();
+
+$mbIntroBottom.on('click touchend', function () {
+	$('html, body').scrollTop(0).scrollLeft(0);
+	$mbIntro.css('position', 'absolute');
+	$mbIntroBottom.delay(50).slideDown(475);
+	$mbIntro.delay(50).slideUp(550);
 	$menu.slicknav(); //Initialize slicknav on header close
+	$carousel.show();
+	$carousel.slick($carousel_val);
+
 });
+
+} else {
+	$carousel.show();
+	$menu.slicknav();
+	$carousel.slick($carousel_val);
+}
+
+
+
